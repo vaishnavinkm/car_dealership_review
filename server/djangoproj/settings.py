922027845f432e7e8dc2eea0c593f8f71bd73902
vaishnavinkm <vaishnavinikam1999@gmail.com>
2026-03-28 15:05:42 +0530

    fix settings

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = ['https://car-dealership-review-2.onrender.com']
 #ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://vaishnavinik-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01."
-    "proxy.cognitiveclass.ai",
+    "https://car-dealership-review-2.onrender.com",
+    #"https://vaishnavinik-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01."
+    #"proxy.cognitiveclass.ai",
 ]
 
 REST_FRAMEWORK = {
