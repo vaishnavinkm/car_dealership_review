6382199677de6fb469b4f0653092195ce7b8e2eb
vaishnavinkm <vaishnavinikam1999@gmail.com>
2026-03-31 17:55:47 +0530

    fix django urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,30 +10,30 @@ urlpatterns = [
     # path for registration
     path("register", views.registration, name="register"),
     # path for login
-    path(route="login", view=views.login_user, name="login"),
+    path("login", views.login_user, name="login"),
     # path for logout
     path("logout/", views.logout_request, name="logout"),
     # path for get cars
-    path(route="get_cars", view=views.get_cars, name="getcars"),
+    path("get_cars", views.get_cars, name="getcars"),
     # path for dealer view
-    path(route="get_dealers/", view=views.get_dealerships, name="get_dealers"),
+    path("get_dealers/", views.get_dealerships, name="get_dealers"),
     path(
-        route="get_dealers/<str:state>",
-        view=views.get_dealerships,
-        name="get_dealers_by_state",
+           "get_dealers/<str:state>",
+            views.get_dealerships,
+            name="get_dealers_by_state",
     ),
     # path for dealer review view
     path(
-        route="reviews/dealer/<int:dealer_id>",
-        view=views.get_dealer_reviews,
-        name="dealer_details",
+            "reviews/dealer/<int:dealer_id>",
+            views.get_dealer_reviews,
+            name="dealer_details",
     ),
     # path for add a review view
-    path(route="add_review", view=views.add_review, name="add_review"),
+    path("add_review", views.add_review, name="add_review"),
     # path for get dealer details
     path(
-        route="dealer/<int:dealer_id>",
-        view=views.get_dealer_details,
-        name="dealer_details",
+            "dealer/<int:dealer_id>",
+            views.get_dealer_details,
+            name="dealer_details",
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
