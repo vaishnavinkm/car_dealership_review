3a6cf9f64a1e70002ffd47f161b949abb6faad38
vaishnavinkm <vaishnavinikam1999@gmail.com>
2026-04-26 15:46:17 +0530

    Increase max_length for car type

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -46,7 +46,7 @@ class CarModel(models.Model):
         ('CONVERTIBLE', 'Convertible'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
         default=2023,
         validators=[MaxValueValidator(2023), MinValueValidator(2015)]
