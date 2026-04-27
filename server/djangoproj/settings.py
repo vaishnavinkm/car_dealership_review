836df1b7a657d6f9b2f28f04b8321e1251073490
vaishnavinkm <vaishnavinikam1999@gmail.com>
2026-04-27 14:24:12 +0530

    Fix Home.html paths and template routing

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -99,7 +99,7 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             os.path.join(BASE_DIR, "frontend/build"),
-            #os.path.join(BASE_DIR, "frontend/static"),
+            os.path.join(BASE_DIR, "frontend/static"),
             #os.path.join(BASE_DIR, "frontend/build/static"),
         ],
         "APP_DIRS": True,
